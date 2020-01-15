90959cca7d508184d6357b59f65186a5f1800f72
dmitry.vorobiev <mustcheat@outlook.com>
2020-01-15 22:01:32 +0300

    upd train script

--- experiments/car_pose_6dof.sh
+++ experiments/car_pose_6dof.sh
@@ -1,11 +1,12 @@
 cd src
 # train
 python main.py car_pose_6dof --exp_id car_pose_default --dataset kaggle_cars \
- --batch_size 12 --master_batch_size 6 \
+ --batch_size 7 --master_batch_size 3 \
  --num_epochs 20 --lr 7e-5 --lr_step 10,15 \
+ --arch hourglass \
  --use_swa --swa_start 10 --swa_freq 5 \
  --mixed_precision --opt_level O1 --max_loss_scale 8192 \
  --flip -1 --aug_shift -1 --shift 0.04 \
  --center_thresh 0.25 --peak_thresh 0.25 --vis_thresh 0.3 \
- --gpus 0,1 --debug 4
+ --gpus 0,1 --debug 0
 cd ..
